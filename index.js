a0eaf05605c0c08b070096f0e929254326bb8159
Alex McHardy <amchardy@shutterstock.com>
2015-09-09 16:20:40 -0600

    Add initial plugin with tests

---
+++ index.js
@@ -0,0 +1,164 @@
+var postcss = require('postcss');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+/**
+ * Trims whitespace and quotes from css 'url()' values
+ *
+ * @param {string} value - string to trim
+ * @returns {string} - the trimmed string
+ */
+function trimUrlValue(value) {
+    var beginSlice, endSlice;
+    value = value.trim();
+    beginSlice = value.charAt(0) === '\'' || value.charAt(0) === '"' ? 1 : 0;
+    endSlice = value.charAt(value.length - 1) === '\'' ||
+        value.charAt(value.length - 1) === '"' ?
+        -1 : undefined;
+    return value.slice(beginSlice, endSlice).trim();
+}
+
+/**
+ * Finds the base dir common to two paths.
+ * For example '/a/b/c/d' and '/a/b/c/x/y' have '/a/b/c' in common.
+ *
+ * @param {string} a - path a
+ * @param {string} b - path b
+ * @returns {string} - the base dir common to both paths
+ */
+function getCommonBaseDir(a, b) {
+    var common = [];
+    a = a.split(path.sep);
+    b = b.split(path.sep);
+    for (var i = 0; i < a.length; i++) {
+        if (b[i] === undefined || b[i] !== a[i]) {
+            break;
+        }
+        common.push(a[i]);
+    }
+    return common.join(path.sep);
+}
+
+/**
+ * Processes a Declaration containing 'url()'
+ *
+ * @param {object} decl - PostCSS Declaration
+ * @param {string} assetBase - Base path to copy assets to
+ * @param {object} opts - options passed to PostCSS
+ * @param {object} postCssResult - PostCSS Result
+ * @returns {void}
+ */
+function handleUrlDecl(decl, assetBase, opts, postCssResult) {
+    // Replace 'url()' parts of Declaration
+    decl.value = decl.value.replace(/url\((.*?)\)/g,
+        function (fullMatch, urlMatch) {
+            // Example:
+            //   decl.value = 'background: url("../../images/foo.png?a=123");'
+            //   urlMatch   = '"../../images/foo.png?a=123"'
+            //   assetBase  = 'dist/assets'
+            //   opts.from  = 'src/css/page/home.css'
+            //   opts.to    = 'dist/assets/css/home.min.css'
+
+            // "../../images/foo.png?a=123" -> ../../images/foo.png?a=123
+            urlMatch = trimUrlValue(urlMatch);
+
+            // Ignore absolute urls, data URIs, or hashes
+            if (urlMatch.indexOf('/') === 0 ||
+                urlMatch.indexOf('data:') === 0 ||
+                urlMatch.indexOf('#') === 0 ||
+                /^[a-z]+:\/\//.test(urlMatch)) {
+                return fullMatch;
+            }
+
+            // '/path/to/project/src/css/page/'
+            var cssFromDirAbs = path.dirname(path.resolve(opts.from));
+            // '/path/to/project/dist/assets/css/page/'
+            var cssToDir = path.dirname(opts.to);
+            // parsed.pathname = '../../images/foo.png'
+            var assetUrlParsed = url.parse(urlMatch);
+            // '/path/to/project/src/images/foo.png'
+            var assetFromAbs =
+                path.resolve(cssFromDirAbs, assetUrlParsed.pathname);
+            // 'foo.png'
+            var assetBasename = path.basename(assetUrlParsed.pathname);
+            // '/path/to/project/src/images'
+            var assetFromDirAbs = path.dirname(assetFromAbs);
+            // '/path/to/project/src'
+            var fromBaseDirAbs =
+                getCommonBaseDir(assetFromDirAbs, cssFromDirAbs);
+            // 'images'
+            var assetPathPart = path.relative(fromBaseDirAbs, assetFromDirAbs);
+            // '/path/to/project/dist/assets/images'
+            var newAssetPath = path.join(assetBase, assetPathPart);
+            // 'foo.png?a=123'
+            var urlBasename = assetBasename +
+                (assetUrlParsed.search ? assetUrlParsed.search : '') +
+                (assetUrlParsed.hash ? assetUrlParsed.hash : '');
+            // '../../images/foo.png?a=123'
+            var newUrl = 'url("' +
+                path.join(path.relative(cssToDir, newAssetPath), urlBasename) +
+                '")';
+
+            // Read the original file
+            var contents;
+            try {
+                contents = fs.readFileSync(assetFromAbs);
+            } catch(e) {
+                postCssResult.warn('Can\'t read asset file "' +
+                    assetFromAbs + '". Ignoring.', { node: decl });
+                return newUrl;
+            }
+
+            // Create any new directories
+            try {
+                mkdirp.sync(newAssetPath);
+            } catch(e) {
+                postCssResult.warn('Can\'t create new asset dir "' +
+                    newAssetPath + '". Ignoring.', { node: decl });
+                return newUrl;
+            }
+
+            // '/path/to/project/dist/assets/images/foo.png'
+            var newAssetFile = path.join(newAssetPath, assetBasename);
+            try {
+                // Write new asset file to assetBase
+                fs.writeFileSync(newAssetFile, contents);
+            } catch(e) {
+                postCssResult.warn('Can\'t write new asset file "' +
+                    newAssetFile + '". Ignoring.', { node: decl });
+                return newUrl;
+            }
+
+            // Return the new url() string
+            return newUrl;
+        }
+    );
+}
+
+module.exports = postcss.plugin('postcss-copy-assets', function (copyOpts) {
+    copyOpts = copyOpts || {};
+    return function (css, result) {
+        var postCssOpts = result.opts;
+        if (!copyOpts.base) {
+            result.warn('postcss-copy-assets requires "base" option so it ' +
+                'knows where to copy assets.');
+            return;
+        }
+        if (!postCssOpts.from) {
+            result.warn('postcss-copy-assets requires postcss "from" option.');
+            return;
+        }
+        if (!postCssOpts.to || postCssOpts.to === postCssOpts.from) {
+            result.warn('postcss-copy-assets requires postcss "to" option.');
+            return;
+        }
+        var assetBase = path.resolve(copyOpts.base);
+        css.walkDecls(function (decl) {
+            if (decl.value && decl.value.indexOf('url(') > -1) {
+                handleUrlDecl(decl, assetBase, postCssOpts, result);
+            }
+        });
+    };
+});
---
+++ test/test.js
@@ -0,0 +1,313 @@
+var mockFs  = require('mock-fs');
+var postcss = require('postcss');
+var expect  = require('chai').expect;
+var path  = require('path');
+var fs = require('fs');
+
+var plugin = require('../');
+
+/* eslint-disable key-spacing */
+var fsFixture = {
+    test: {
+        fixtures: {
+            'test5.png': '5',
+            src: {
+                'test2.png': '2',
+                images: {
+                    'test3.png': '3',
+                    logos: {
+                        'test4.png': '4',
+                        'test6.png': '6'
+                    }
+                },
+                css: {
+                    'test1.png': '1'
+                }
+            },
+            images: {
+                'test6.png': '6'
+            }
+        }
+    }
+};
+/* eslint-enable key-spacing */
+
+
+/**
+ * Tests plugin with given options
+ *
+ * @param {string} input - Input css
+ * @param {string} output - Expected output css
+ * @param {string[]} outputFiles - Expected output files to be copied
+ * @param {object} opts - Options to pass along
+ * @param {object} opts.postcss - Options for PostCSS
+ * @param {object} opts.plugin - Options for plugin itself
+ * @param {number} warningsCount - Expected number of PostCSS warnings
+ * @param {function} done - Async callback
+ * @returns {void}
+ */
+var test = function (input, output, outputFiles, opts, warningsCount, done) {
+    postcss([ plugin(opts.plugin) ]).process(input, opts.postcss)
+        .then(function (result) {
+            var warnings = result.warnings();
+            expect(result.css).to.eql(output);
+            if (warnings.length && warnings.length !== warningsCount) {
+                console.log(warnings.join('\n'));
+            }
+            expect(warnings.length, warningsCount + ' warning(s) expected')
+                .to.be.equal(warningsCount);
+            if (outputFiles) {
+                outputFiles.forEach(function (filepath) {
+                    var outputExists = fs.existsSync(path.resolve(filepath));
+                    if (!outputExists) {
+                        console.log(fs.readdirSync(path.dirname(filepath)));
+                    }
+                    expect(outputExists, filepath + ' exists').to.be.true;
+                });
+            }
+            done();
+        }).catch(function (error) {
+            done(error);
+        });
+};
+
+describe('postcss-copy-assets', function () {
+
+    var opts;
+
+    beforeEach(function () {
+        mockFs(fsFixture);
+        opts = {
+            postcss: {
+                from: 'test/fixtures/src/css/test.css',
+                to:   'test/dist/assets/css/test.css'
+            },
+            plugin: {
+                base: 'test/dist/assets'
+            }
+        };
+    });
+
+    afterEach(function () {
+        mockFs.restore();
+    });
+
+    it('warns if no "from" option provided', function (done) {
+        delete opts.postcss.from;
+        test('a{ background: url("foo") }',
+             'a{ background: url("foo") }',
+             null,
+             opts, 1, done);
+    });
+
+    it('warns if no "to" option provided', function (done) {
+        delete opts.postcss.to;
+        test('a{ background: url("foo") }',
+             'a{ background: url("foo") }',
+             null,
+             opts, 1, done);
+    });
+
+    it('warns if no "base" option provided', function (done) {
+        delete opts.plugin;
+        test('a{ background: url("foo") }',
+             'a{ background: url("foo") }',
+             null,
+             opts, 1, done);
+    });
+
+    it('warns if asset file not there', function (done) {
+        test('a{ background: url("test-not-there.png") }',
+             'a{ background: url("../test-not-there.png") }',
+             null,
+             opts, 1, done);
+    });
+
+    it('ignores root-relative url()', function (done) {
+        test('a{ background: url("/test/fixtures/src/images/test3.png") }',
+             'a{ background: url("/test/fixtures/src/images/test3.png") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('ignores http absolute url()', function (done) {
+        test('a{ background: url("http://example.com/images/test3.png") }',
+             'a{ background: url("http://example.com/images/test3.png") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('ignores https absolute url()', function (done) {
+        test('a{ background: url("https://example.com/images/test3.png") }',
+             'a{ background: url("https://example.com/images/test3.png") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('ignores file absolute url()', function (done) {
+        test('a{ background: url("file:///src/images/test3.png") }',
+             'a{ background: url("file:///src/images/test3.png") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('ignores protocol-relative absolute url()', function (done) {
+        test('a{ background: url("//example.com/images/test3.png") }',
+             'a{ background: url("//example.com/images/test3.png") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('ignores data URI url()', function (done) {
+        test('a{ background: url("data:foo;abcd") }',
+             'a{ background: url("data:foo;abcd") }',
+             null,
+             opts, 0, done);
+    });
+
+    it('handles asset in same dir as css', function (done) {
+        test('a{ background: url("test1.png") }',
+             'a{ background: url("../test1.png") }',
+             ['test/dist/assets/test1.png'],
+             opts, 0, done);
+    });
+
+    it('handles asset in parent dir from css', function (done) {
+        test('a{ background: url("../test2.png") }',
+             'a{ background: url("../test2.png") }',
+             ['test/dist/assets/test2.png'],
+             opts, 0, done);
+    });
+
+    it('handles asset in different tree from css', function (done) {
+        test('a{ background: url("../images/test3.png") }',
+             'a{ background: url("../images/test3.png") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles asset in different tree from css with subdirs',
+        function (done) {
+            test('a{ background: url("../images/logos/test4.png") }',
+                 'a{ background: url("../images/logos/test4.png") }',
+                 ['test/dist/assets/images/logos/test4.png'],
+                 opts, 0, done);
+        }
+    );
+
+    it('handles asset outside base dir from css', function (done) {
+        test('a{ background: url("../../test5.png") }',
+             'a{ background: url("../test5.png") }',
+             ['test/dist/assets/test5.png'],
+             opts, 0, done);
+    });
+
+    it('handles asset outside base dir from css with subdirs',
+        function (done) {
+            test('a{ background: url("../../images/test6.png") }',
+                 'a{ background: url("../images/test6.png") }',
+                 ['test/dist/assets/images/test6.png'],
+                 opts, 0, done);
+        }
+    );
+
+    it('handles asset base dir different tree from css "to" dir',
+        function (done) {
+            opts = {
+                postcss: {
+                    from: 'test/fixtures/src/css/test.css',
+                    to:   'test/dist/assets/css/test.css'
+                },
+                plugin: {
+                    base: 'test/dist/other'
+                }
+            };
+            test('a{ background: url("../images/logos/test4.png") }',
+                 'a{ background: url("../../other/images/logos/test4.png") }',
+                 ['test/dist/other/images/logos/test4.png'],
+                 opts, 0, done);
+        }
+    );
+
+    it('handles css "to" path shorter than "from" path', function (done) {
+        opts.postcss = {
+            from: 'test/fixtures/src/css/page/group/test.css',
+            to:   'test/dist/assets/css/test.css'
+        };
+        test('a{ background: url("../../../images/logos/test4.png") }',
+             'a{ background: url("../images/logos/test4.png") }',
+             ['test/dist/assets/images/logos/test4.png'],
+             opts, 0, done);
+    });
+
+    it('handles css "to" path longer than "from" path', function (done) {
+        opts.postcss = {
+            from: 'test/fixtures/src/css/test.css',
+            to:   'test/dist/assets/css/page/group/test.css'
+        };
+        test('a{ background: url("../images/logos/test4.png") }',
+             'a{ background: url("../../../images/logos/test4.png") }',
+             ['test/dist/assets/images/logos/test4.png'],
+             opts, 0, done);
+    });
+
+    it('handles query string in url()', function (done) {
+        test('a{ background: url("../images/test3.png?foo=abc") }',
+             'a{ background: url("../images/test3.png?foo=abc") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles hash in url()', function (done) {
+        test('a{ background: url("../images/test3.png#bar") }',
+             'a{ background: url("../images/test3.png#bar") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles query string and hash in url()', function (done) {
+        test('a{ background: url("../images/test3.png?foo=abc#bar") }',
+             'a{ background: url("../images/test3.png?foo=abc#bar") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles no quotes in url()', function (done) {
+        test('a{ background: url(../images/test3.png) }',
+             'a{ background: url("../images/test3.png") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles whitespace in url()', function (done) {
+        test('a{ background: url(  "../images/test3.png  "    ) }',
+             'a{ background: url("../images/test3.png") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles single quotes in url()', function (done) {
+        test('a{ background: url( \'  ../images/test3.png\' ) }',
+             'a{ background: url("../images/test3.png") }',
+             ['test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+    it('handles url() alongside other parts of declaration', function (done) {
+        test('a{ background: rgba(1,1,1,1) url("test1.png") #fab no-repeat}',
+             'a{ background: rgba(1,1,1,1) url("../test1.png") #fab no-repeat}',
+             ['test/dist/assets/test1.png'],
+             opts, 0, done);
+    });
+
+    it('handles multiple url()s in same declaration', function (done) {
+        test('a{ background: url("test1.png") url( \'../test2.png \'  ) ' +
+             'url(../images/test3.png) }',
+             'a{ background: url("../test1.png") url("../test2.png") ' +
+             'url("../images/test3.png") }',
+             ['test/dist/assets/test1.png', 'test/dist/assets/test2.png',
+             'test/dist/assets/images/test3.png'],
+             opts, 0, done);
+    });
+
+});
